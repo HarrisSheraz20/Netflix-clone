2da00e968aace890bc733b00541ba285d70f5ce9
Harris Sheraz <58743620+HarrisSheraz20@users.noreply.github.com>
2021-03-04 22:08:56 +0000

    just added firebase hosting

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -23,3 +23,13 @@ function App() {
 }
 
 export default App;
+
+
+// This is the API key to all the movies
+// a704338c2463691df9a73198b0182028
+
+// Example API Request
+// https://api.themoviedb.org/3/movie/550?api_key=a704338c2463691df9a73198b0182028
+
+// API Read Access Token (v4 auth)
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNzA0MzM4YzI0NjM2OTFkZjlhNzMxOThiMDE4MjAyOCIsInN1YiI6IjYwNDBmMDUzN2VjZDI4MDA0MzBkMDNmNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZFXMmvTLpDIqpAocd0qpc3vLqXS6hbEPM25TeEONy9A
