38bb2b67a057e80e3c3aaa991a0c7a8cbc2f7acf
Harris Sheraz <58743620+HarrisSheraz20@users.noreply.github.com>
2021-03-06 15:14:36 +0000

    just added rows of movies

--- netflix-clone/src/Components/Row/Row.js
+++ netflix-clone/src/Components/Row/Row.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect }from 'react'
 import axios from '../../axios'
 import requests from '../../requests';
+import "../Row/Row.css"
+
+
+const base_url = "https://image.tmdb.org/t/p/original/"
 
 function Row({title, fetchUrl}) {
 
@@ -11,18 +15,25 @@ function Row({title, fetchUrl}) {
         // if [], run once when the row loads, and don't run again 
         async function fetchData(){
             const request = await axios.get(fetchUrl);
-            console.log(request)
+            setMovies(request.data.results)
             return request;
         }
         fetchData();
-    }, []);
+    }, [fetchUrl]);
+
+    console.table(movies)
 
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
 
-
-            {/* container --> posters */}
+            <div className="row__posters">
+                {movies.map(movie => (
+                    <img
+                    className="row__poster"
+                    src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+                ))}
+            </div>
         </div>
     )
 }
